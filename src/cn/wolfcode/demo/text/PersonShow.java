082dbe7afd65af952a324690a3944aa21b13d615
LTX <1489731286@qq.com>
2024-04-19 19:16:20 +0800

    third commit

--- src/cn/wolfcode/demo/text/PersonShow.java
+++ src/cn/wolfcode/demo/text/PersonShow.java
@@ -7,6 +7,7 @@ public class PersonShow {
     public static void main(String[] args) {
         System.out.println("词穷了");
         System.out.println("你要干嘛");
+        System.out.println("第三次提交");
     }
 
 }
