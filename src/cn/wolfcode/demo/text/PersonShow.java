83494c807cb985c3fca6a517d841c26c500334db
LTX <1489731286@qq.com>
2024-04-20 17:27:05 +0800

    v commit

--- src/cn/wolfcode/demo/text/PersonShow.java
+++ src/cn/wolfcode/demo/text/PersonShow.java
@@ -4,19 +4,20 @@ import javax.swing.*;
 
 /**
  * 这是一个个人信息展示类
- * */
+ */
 public class PersonShow {
-    //这是一个主函数
-    public static void main(String[] args) {
-        System.out.println("词穷了");
-        System.out.println("你要干嘛");
-        System.out.println("第三次提交");
-        System.out.println("你好，我是master主分支");
-        System.out.println("你好，我是hot-fix分支");
-        System.out.println("hot-fix");
-        System.out.println("My Name is hot-fix");
-        System.out.println("push操作");
-        System.out.println("pull操作");
-    }
+  // 这是一个主函数
+  public static void main(String[] args) {
+    System.out.println("词穷了");
+    System.out.println("你要干嘛");
+    System.out.println("第三次提交");
+    System.out.println("你好，我是master主分支");
+    System.out.println("你好，我是hot-fix分支");
+    System.out.println("hot-fix");
+    System.out.println("My Name is hot-fix");
+    System.out.println("push操作");
+    System.out.println("pull操作");
+    System.out.println("heiheihei 我正在使用vscode进行代码管理");
+  }
 
 }
